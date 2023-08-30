64178fa4f4962ed11873abd156063cba03ea8289
21101133370057 <130560115+21101133370057@users.noreply.github.com>
2023-08-30 11:14:27 +0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link rel="stylesheet" href="login1.css">
+</head>
+<body>
+    
+<form action="insert.php" method="post">
+    <div class="ambwene">
+    <h1>ShakirBackpacks</h1>
+</div>
+    <div class="ambwene1">
+    <h3>Welcome to Shakir Backpacks</h3>
+</div>
+<div class="ambwene2">
+<label for="username">Username or Email</label><br>
+        <input type="text" id="name" name="username" required><br><br>
+        <label for="password">Password</label><br>
+        <input type="password" id="password" name="password" required><br><br>
+        <input type="submit" value="Submit">
+    <h4>Don't have Account</h4>
+    <a href="create.php">Create New Account</a>
+</div>
+  </form>  
+
+</body>
+</html> 
+
+
+
+
